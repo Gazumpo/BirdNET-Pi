b126046799788de465463a9788abb667cc703e9a
Graeme L <clicheguevara1@gmail.com>
2025-10-12 21:11:43 +0800

    changed to bottom bar nav

--- birdnet-ng/src/app/app.ts
+++ birdnet-ng/src/app/app.ts
@@ -1,14 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject, computed } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Header } from './header/header';
 import { Sidebar } from './sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidebar],
+  imports: [RouterOutlet, RouterLink, Header, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('birdnet-ng');
+  private bo = inject(BreakpointObserver);
+
+  readonly isHandset = toSignal(
+    this.bo
+      .observe([Breakpoints.XSmall, Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+      .pipe(map(r => r.matches)),
+    { initialValue: false }
+  );
+
+  readonly isDesktopOrTablet = computed(() => !this.isHandset());
 }
